770c6c9630388acad4b67845c50a89cab79fefdf
Ahmed Joda <ahmedjodaalmory99@gmail.com>
2020-11-03 15:41:08 +0200

    github pages

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -16,21 +16,7 @@ const Footer = () => {
             </p>
           </MDBCol>
           <hr className="w-100 clearfix d-md-none" />
-          <MDBCol md="2" lg="2" xl="2" className="mx-auto mt-3">
-            <h6 className="text-uppercase mb-4 font-weight-bold">Products</h6>
-            <p>
-              <a href="#!">MDBootstrap</a>
-            </p>
-            <p>
-              <a href="#!">MDWordPress</a>
-            </p>
-            <p>
-              <a href="#!">BrandFlow</a>
-            </p>
-            <p>
-              <a href="#!">Bootstrap Angular</a>
-            </p>
-          </MDBCol>
+
           <hr className="w-100 clearfix d-md-none" />
           <MDBCol md="3" lg="2" xl="2" className="mx-auto mt-3">
             <h6 className="text-uppercase mb-4 font-weight-bold">
@@ -53,16 +39,13 @@ const Footer = () => {
           <MDBCol md="4" lg="3" xl="3" className="mx-auto mt-3">
             <h6 className="text-uppercase mb-4 font-weight-bold">Contact</h6>
             <p>
-              <i className="fa fa-home mr-3" /> New York, NY 10012, US
+              <i className="fa fa-home mr-3" /> 6th of October
             </p>
             <p>
-              <i className="fa fa-envelope mr-3" /> info@gmail.com
+              <i className="fa fa-envelope mr-3" /> mail@gmail.com
             </p>
             <p>
-              <i className="fa fa-phone mr-3" /> + 01 234 567 88
-            </p>
-            <p>
-              <i className="fa fa-print mr-3" /> + 01 234 567 89
+              <i className="fa fa-phone mr-3" /> +21211779758
             </p>
           </MDBCol>
         </MDBRow>
